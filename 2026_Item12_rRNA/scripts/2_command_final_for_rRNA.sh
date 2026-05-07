e883ca124ba0a5558862666c53a717af69c5090b
ZG_Tailscale <bioinfo.gao@gmail.com>
2026-05-07 16:48:00 -0400

    Rename folder from 2026_Item11_Mingxue to 2026_Item11_Mingxue_with_2_pairs_samples to reflect 2 pairs of samples

--- 2026_Item12_rRNA/scripts/2_command_final_for_rRNA.sh
+++ 2026_Item12_rRNA/scripts/2_command_final_for_rRNA.sh
@@ -17,36 +17,6 @@ nextflow -version
 tmux new -s RNA5  
 # tmux a
 
-# 限制 Nextflow 自身的内存开销，确保它不被 Killed
-export NXF_OPTS="-Xms512m -Xmx2g"
-# 设置 Singularity 缓存目录以避免重复下载容器镜像
-export NXF_SINGULARITY_CACHEDIR="/home/gao/.singularity/nf-core"
-
-cd /Work_bio/gao/projects/2026_Item12_rRNAal/scripts
-
-nextflow run nf-core/rnaseq \
-    -r 3.15.1 \
-    -profile singularity \
-    -c local_optimized.config \
-    -c avoid_download.config \
-    --input /home/gao/projects/2026_Item12_rRNAal/scripts/Sample_Sheet2.csv \
-    --outdir ../output_results \
-    --fasta /Work_bio/references/Homo_sapiens/GRCh38/human_gencode_v45/GRCh38.primary_assembly.genome.fa \
-    --gtf /Work_bio/references/Homo_sapiens/GRCh38/human_gencode_v45/gencode.v45.annotation.gtf \
-    --star_index /Work_bio/references/Homo_sapiens/GRCh38/human_gencode_v45/star_index \
-    --gencode \
-    --aligner star_salmon \
-    --remove_ribo_rna \
-    --ribo_database_manifest "$RIBO_MANIFEST" \
-    --save_non_ribo_reads \
-    --max_cpus 28 \
-    --max_memory '90.GB'
-
-    # 暂时移除 -resume # 参数
-
-# 2026-04-26 修改 -resume  Ctrl C
-tmux a 
-
 cd /Work_bio/gao/projects/2026_Item12_rRNAal/scripts
 
 # 限制 Nextflow 自身的内存开销，确保它不被 Killed
@@ -57,13 +27,14 @@ export NXF_SINGULARITY_CACHEDIR="/home/gao/.singularity/nf-core"
 # Check if rRNA databases exist locally
 RIBO_MANIFEST="/Work_bio/gao/projects/2026_Item12_rRNAal/scripts/rRNA_databases/sortmerna_database_manifest.txt"
 
+# 2026-05-05 添加  改回同时跑两个样本，MEM 90G
 
 nextflow run nf-core/rnaseq \
     -r 3.15.1 \
     -profile singularity \
     -c local_optimized.config \
     -c avoid_download.config \
-    --input /home/gao/projects/2026_Item12_rRNAal/scripts/Sample_Sheet2.csv \
+    --input /home/gao/projects/2026_Item12_rRNAal/scripts/Samples.csv \
     --outdir ../output_results \
     --fasta /Work_bio/references/Homo_sapiens/GRCh38/human_gencode_v45/GRCh38.primary_assembly.genome.fa \
     --gtf /Work_bio/references/Homo_sapiens/GRCh38/human_gencode_v45/gencode.v45.annotation.gtf \
@@ -75,5 +46,5 @@ nextflow run nf-core/rnaseq \
     --save_non_ribo_reads \
     --max_cpus 28 \
     --max_memory '90.GB' \
-    -resume  #参数    
+    -resume  #参数         # 暂时移除 -resume # 参数, 还有上一行 末尾的 \
     
